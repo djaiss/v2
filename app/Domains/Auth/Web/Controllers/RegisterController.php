ce036e797f215d247f5027f1945a8b05d9fb1328
Regis Freyd <djaiss@users.noreply.github.com>
2023-02-24 13:19:36 -0500

    Update RegisterController.php

--- app/Domains/Auth/Web/Controllers/RegisterController.php
+++ app/Domains/Auth/Web/Controllers/RegisterController.php
@@ -2,15 +2,8 @@
 
 namespace App\Domains\Auth\Web\Controllers;
 
-use App\Helpers\WallpaperHelper;
 use App\Http\Controllers\Controller;
-use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cookie;
-use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
-use Inertia\Response;
-use LaravelWebauthn\Facades\Webauthn;
 use Illuminate\View\View;
 
 class RegisterController extends Controller
@@ -22,6 +15,5 @@ class RegisterController extends Controller
 
     public function store(Request $request)
     {
-
     }
 }
